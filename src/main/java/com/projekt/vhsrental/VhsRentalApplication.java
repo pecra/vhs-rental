30f2e8467da030b9448f1173db664da32e0418c9
petra renic <petra.renic@gmail.com>
2025-12-05 18:19:47 +0100

    Pocetni setup projekta

---
+++ src/main/java/com/projekt/vhsrental/VhsRentalApplication.java
@@ -0,0 +1,13 @@
+package com.projekt.vhsrental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VhsRentalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VhsRentalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/projekt/vhsrental/VhsRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projekt.vhsrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VhsRentalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
